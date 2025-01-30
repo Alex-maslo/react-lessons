623a02b1dc7fa63a50b9ab31e8eae8f1ed320f3f
Olexandr <4439897@gmail.com>
2025-01-30 21:04:21 +0200

    [lesson-5]:task-3

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,26 +1,63 @@
-import { FormEvent } from "react";
+import { useForm } from "react-hook-form";
+
+interface IFormProps {
+  username: string;
+  password: string;
+  age: number;
+}
 
 const FormComponent = () => {
-  const handleChange = (e: FormEvent<HTMLInputElement>) => {
-    console.log(e);
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isValid },
+  } = useForm<IFormProps>({ mode: "all" });
+
+  const customHandleSubmit = (formDataProps: IFormProps) => {
+    console.log(formDataProps);
   };
+
   return (
     <div>
-      <form className="form">
-        <input
-          className={`border border-orange-600 p-2 m-2`}
-          type="text"
-          name={"username"}
-          placeholder="Username"
-          onChange={handleChange}
-        />
-        <input
-          className={`border border-orange-600 p-2 m-2 `}
-          type="text"
-          name={"password"}
-          placeholder="Password"
-        />
-        <button className={`border border-orange-600 p-2 m-2 `}>send</button>
+      <form onSubmit={handleSubmit(customHandleSubmit)}>
+        <label>
+          {" "}
+          <input
+            type="text"
+            {...register("username", {
+              pattern: { value: /\w+/, message: "wrong name" },
+              required: true,
+              minLength: { value: 1, message: "wrong name" },
+            })}
+          />
+          {errors.username && <div>{errors.username.message}</div>}
+        </label>
+
+        <label>
+          <input
+            type="text"
+            {...register("password", {
+              required: true,
+              minLength: { value: 3, message: "pass to short" },
+              maxLength: { value: 6, message: "pass to long" },
+            })}
+          />
+
+          {errors.password && <div>{errors.password.message}</div>}
+        </label>
+
+        <label>
+          <input
+            {...register("age", {
+              required: true,
+              valueAsNumber: true,
+              min: { value: 1, message: "age to small" },
+              max: { value: 117, message: "age to big" },
+            })}
+          />
+          {errors.age && <div>{errors.age.message}</div>}
+        </label>
+        <button disabled={!isValid}>send</button>
       </form>
     </div>
   );
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,7 +3,6 @@ import FormComponent from "../components/FormComponent.tsx";
 export const MainLayout = () => {
   return (
     <div>
-      Hello
       <FormComponent />
     </div>
   );
