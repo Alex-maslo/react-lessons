a8b08690020a1b03928aae08a1282974fa778fe7
Olexandr <4439897@gmail.com>
2025-02-02 12:03:16 +0200

    [lesson-6]:task-3

--- src/pages/AuthResourcesPage.tsx
+++ src/pages/AuthResourcesPage.tsx
@@ -1,11 +1,21 @@
-import { loadAuthProducts } from "../services/api.service.ts";
+import { loadAuthProducts, refresh } from "../services/api.service.ts";
 import { useEffect } from "react";
 
 export const AuthResourcesPage = () => {
   useEffect(() => {
-    loadAuthProducts().then((products) => {
-      console.log(products);
-    });
+    loadAuthProducts()
+      .then((products) => {
+        console.log(products);
+      })
+      .catch((reason) => {
+        console.log(reason);
+
+        refresh()
+          .then(() => loadAuthProducts())
+          .then((value) => {
+            console.log(value);
+          });
+      });
   }, []);
   return <div>AuthResourcesPage</div>;
 };
--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { IUserWithTokens } from "../models/IUserWithTokens.ts";
 import { IProduct } from "../models/IProduct.ts";
 import { IProductsResponseModelType } from "../models/IProductsResponseModelType.ts";
-import { retriveLocalStorage } from "./helpers.ts";
+import { ITokenPair } from "../models/ITokenPair.ts";
 
 type LoginData = {
   username: string;
@@ -15,10 +15,19 @@ const axiosInstance = axios.create({
   headers: {},
 });
 
+const retriveLocalStorage = <T>(key: string) => {
+  const object = localStorage.getItem(key) || "";
+  if (!object) {
+    return {} as T;
+  }
+  const parse = JSON.parse(object).accessToken;
+  return parse as T;
+};
+
 axiosInstance.interceptors.request.use((requestObject) => {
-  if (requestObject.method?.toUpperCase() === "GET") {
-    requestObject.headers.authorization =
-      "Bearer " + retriveLocalStorage<IUserWithTokens>("user").accessToken;
+  if (requestObject.method?.toLocaleUpperCase() === "GET") {
+    requestObject.headers.Authorization =
+      "Bearer " + retriveLocalStorage("user");
   }
 
   return requestObject;
@@ -37,14 +46,29 @@ export const login = async ({
       expiresInMins,
     },
   );
-  console.log(userWithTokens);
   localStorage.setItem("user", JSON.stringify(userWithTokens));
+  console.log(userWithTokens);
   return userWithTokens;
 };
 
 export const loadAuthProducts = async (): Promise<IProduct[]> => {
+  const { data } =
+    await axiosInstance.get<IProductsResponseModelType>("/products");
+  return data.products;
+};
+
+export const refresh = async () => {
+  const iUserWithTokens = retriveLocalStorage<IUserWithTokens>("user");
   const {
-    data: { products },
-  } = await axiosInstance.get<IProductsResponseModelType>("/products/");
-  return products;
+    data: { accessToken, refreshToken },
+  } = await axiosInstance.post<ITokenPair>("/refresh", {
+    refreshToken: iUserWithTokens.refreshToken,
+    expiresInMins: 1,
+  });
+
+  console.log(accessToken);
+  console.log(refreshToken);
+  iUserWithTokens.accessToken = accessToken;
+  iUserWithTokens.refreshToken = refreshToken;
+  localStorage.setItem("user", JSON.stringify(iUserWithTokens));
 };
